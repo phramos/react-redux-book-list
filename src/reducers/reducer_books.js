88560d4802ea45b5a8ef0baf7b333e5d7ffa9983
Pedro Souza <pedro@vencis.com.br>
2016-12-01 16:20:02 -0200

    Connecting book-list to reducer_book

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title:'Javascript: The Good Parts'},
+        {title:'Eloquent Ruby'},
+        {title:'The Dark Tower'},
+        {title:'The Lord of the Rings'},
+        {title:'Head First'}
+    ];
+}
